b8c7da72d0aaaa20ae3efab62b454bf32984b17c
StarlightSora <anonymous@example.com>
2026-07-26 00:00:20 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::{time::Instant};
+
+use hooke_spring::HookeSpring;
+
+fn main() {
+    let mut spring = HookeSpring::new(
+        Some(0.0),   // position
+        Some(0.0),   // velocity
+        Some(1.0),   // target
+        Some(0.75),  // damper (defaults to 1.0)
+        Some(2.0),  // speed (defaults to 1.0)
+        None,  // clock (defaults to Box::new(SmolStopwatch::new()))
+    );
+
+    // Kick the spring with an impulse
+    spring.impulse(5.0);
+
+    // Print a few steps
+    let now = Instant::now();
+    for _ in 0..10 {
+        let (pos, vel) = spring.get_position_and_velocity();
+        println!("t+:{}ms: pos = {}, vel = {}", now.elapsed().as_millis(), pos, vel);
+        std::thread::sleep(std::time::Duration::from_millis(200));
+    }
+}
