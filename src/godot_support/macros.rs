1d619b38155ed6b7ec74b2713fad25f321bcdf5b
StarlightSora <anonymous@example.com>
2026-07-28 22:26:01 +0900

    add getter methods and a clock_mut function for RSHookeSpring; enhance macros for better type handling

--- src/godot_support/macros.rs
+++ src/godot_support/macros.rs
@@ -1,9 +1,9 @@
 //! You are entering the realm of black magic spells;
-//! "dirty" macro hacks because Rust is overly strict
+//! dirty(?) macro hacks because Rust is overly strict
 
-// Is there really no better way to handle these? Seriously?
+// Is there really no better way to handle these? Seriously? This is idiomatic Rust?
 
-// Assign necessary traits to newtypes; when multiplying $inner with f64, cast to $mul_precision for compatibility (typically f64 or f32)
+/// Assign necessary traits to newtypes; when multiplying $inner with f64, cast to $mul_precision for compatibility (typically f64 or f32)
 // TODO: Maybe split these? So the entire macro doesn't shoot itself on the foot if the original type doesn't have a trait here??
 // TODO 2: Move this to src/macros.rs instead, and enable access to external crates to this macro?
 macro_rules! assign_spring_compat_traits {
@@ -51,75 +51,119 @@ macro_rules! assign_spring_compat_traits {
 }
 pub(super) use assign_spring_compat_traits;
 
-// Shorthand for branching logic when calling operations on springs
-// For a given HookeSpring<T> instance when $primary is type T
+/// Shorthand for branching logic when calling operations on RSHookeSpringVariants
+/// For a given HookeSpring<HSCompatibleTypes> instance when $primary is type HSCompatibleTypes
 macro_rules! hs_variant_match_typed {
     ($spr: expr, $method: expr, $primary: expr $(, $extra: expr)*) => {
         match ($spr, $primary) {
             (RSHookeSpringVariant::Float(spring), HSCompatibleTypes::Float(val)) => {
-                $method(spring, val$(, $extra)*);
+                $method(spring, val$(, $extra)*)
             }
             (RSHookeSpringVariant::Vector2(spring), HSCompatibleTypes::Vector2(val)) => {
-                $method(spring, val$(, $extra)*);
+                $method(spring, val$(, $extra)*)
             }
             (RSHookeSpringVariant::Vector3(spring), HSCompatibleTypes::Vector3(val)) => {
-                $method(spring, val$(, $extra)*);
+                $method(spring, val$(, $extra)*)
             }
             (RSHookeSpringVariant::Vector4(spring), HSCompatibleTypes::Vector4(val)) => {
-                $method(spring, val$(, $extra)*);
+                $method(spring, val$(, $extra)*)
             }
             _ => {
-                godot_error!("Type mismatch!");
+                godot_error!("Type mismatch!")
             }
         }
     };
 }
 pub(super) use hs_variant_match_typed;
 
-// Shorthand for branching logic when calling operations on springs
-// For a given HookeSpring<T> instance when $primary and $secondary are type T
+/// Shorthand for branching logic when calling operations on RSHookeSpringVariants
+/// For a given HookeSpring<HSCompatibleTypes> instance when $primary and $secondary are type HSCompatibleTypes
 // Literally only used for one case but whatever
 macro_rules! hs_variant_match_double_typed {
     ($spr: expr, $method: expr, $primary: expr, $secondary: expr $(, $extra: expr)*) => {
         match ($spr, $primary, $secondary) {
             (RSHookeSpringVariant::Float(spring), HSCompatibleTypes::Float(val), HSCompatibleTypes::Float(val2)) => {
-                $method(spring, val, val2$(, $extra)*);
+                $method(spring, val, val2$(, $extra)*)
             }
             (RSHookeSpringVariant::Vector2(spring), HSCompatibleTypes::Vector2(val), HSCompatibleTypes::Vector2(val2)) => {
-                $method(spring, val, val2$(, $extra)*);
+                $method(spring, val, val2$(, $extra)*)
             }
             (RSHookeSpringVariant::Vector3(spring), HSCompatibleTypes::Vector3(val), HSCompatibleTypes::Vector3(val2)) => {
-                $method(spring, val, val2$(, $extra)*);
+                $method(spring, val, val2$(, $extra)*)
             }
             (RSHookeSpringVariant::Vector4(spring), HSCompatibleTypes::Vector4(val), HSCompatibleTypes::Vector4(val2)) => {
-                $method(spring, val, val2$(, $extra)*);
+                $method(spring, val, val2$(, $extra)*)
             }
             _ => {
-                godot_error!("Type mismatch!");
+                godot_error!("Type mismatch!")
             }
         }
     };
 }
 pub(super) use hs_variant_match_double_typed;
 
-// Shorthand for branching logic when calling operations on springs
-// For a given HookeSpring<T> instance when $primary is NOT type T
+/// Shorthand for branching logic when calling operations on RSHookeSpringVariants
+/// For a given HookeSpring<HSCompatibleTypes> instance when all the parameters are NOT type HSCompatibleTypes
 macro_rules! hs_variant_match_untyped {
-    ($spr: expr, $method: expr, $primary: expr $(, $extra: expr)*) => {
+    ($spr: expr, $method: expr $(, $extra: expr)*) => {
         match ($spr) {
             RSHookeSpringVariant::Float(spring) => {
-                $method(spring, $primary$(, $extra)*);
+                $method(spring$(, $extra)*)
             }
             RSHookeSpringVariant::Vector2(spring) => {
-                $method(spring, $primary$(, $extra)*);
+                $method(spring$(, $extra)*)
             }
             RSHookeSpringVariant::Vector3(spring) => {
-                $method(spring, $primary$(, $extra)*);
+                $method(spring$(, $extra)*)
             }
             RSHookeSpringVariant::Vector4(spring) => {
-                $method(spring, $primary$(, $extra)*);
+                $method(spring$(, $extra)*)
             }
         }
     };
 }
 pub(super) use hs_variant_match_untyped;
+
+/// Shorthand for branching logic when calling operations on RSHookeSpringVariants
+/// For a given HookeSpring<HSCompatibleTypes> instance where it is expected to return **one of** HSCompatibleTypes type
+macro_rules! hs_variant_getter_generic_deref {
+    ($spr: expr, $method: expr) => {
+        match ($spr) {
+            RSHookeSpringVariant::Float(spring) => {
+                HSCompatibleTypes::Float(*$method(spring))
+            }
+            RSHookeSpringVariant::Vector2(spring) => {
+                HSCompatibleTypes::Vector2(*$method(spring))
+            }
+            RSHookeSpringVariant::Vector3(spring) => {
+                HSCompatibleTypes::Vector3(*$method(spring))
+            }
+            RSHookeSpringVariant::Vector4(spring) => {
+                HSCompatibleTypes::Vector4(*$method(spring))
+            }
+        }
+    };
+    // $tuple is literally just a placeholder value to match this specific case
+    // it means absolutely nothing otherwise
+    ($spr: expr, $method: expr, $tuple: literal) => {
+        match ($spr) {
+            RSHookeSpringVariant::Float(spring) => {
+                let tup = $method(spring);
+                (HSCompatibleTypes::Float(*tup.0), HSCompatibleTypes::Float(*tup.1))
+            }
+            RSHookeSpringVariant::Vector2(spring) => {
+                let tup = $method(spring);
+                (HSCompatibleTypes::Vector2(*tup.0), HSCompatibleTypes::Vector2(*tup.1))
+            }
+            RSHookeSpringVariant::Vector3(spring) => {
+                let tup = $method(spring);
+                (HSCompatibleTypes::Vector3(*tup.0), HSCompatibleTypes::Vector3(*tup.1))
+            }
+            RSHookeSpringVariant::Vector4(spring) => {
+                let tup = $method(spring);
+                (HSCompatibleTypes::Vector4(*tup.0), HSCompatibleTypes::Vector4(*tup.1))
+            }
+        }
+    };
+}
+pub(super) use hs_variant_getter_generic_deref;
