b2f769dce47d0c76ee47a8fa5eaec833e1f128ff
StarlightSora <300897738+StarlightSora@users.noreply.github.com>
2026-07-29 15:47:37 +0900

    Fix unused import warning

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,7 @@ pub use hooke_spring::HookeSpring;
 
 pub mod hooke_spring;
 pub mod clocks;
+pub mod macros; // Needs to be pub to suppress unused import warning
 
 #[cfg(feature = "godot_bind")]
 pub mod godot_support;
-
-mod macros;
