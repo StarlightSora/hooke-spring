bc55fa71e20595cd06f8723f953c6cfe633afd8d
StarlightSora <300897738+StarlightSora@users.noreply.github.com>
2026-08-03 21:46:00 +0900

    readme change, fixed integration with godot

--- src/godot_bind/mod.rs
+++ src/godot_bind/mod.rs
@@ -1,11 +1,20 @@
 //! Contains implementations specifically for Godot Engine.
 //! Primarily intended to be used within GDScript.
 //! 
-//! For examples, please refer to `/examples/godot_bind` in the source code
+//! To properly register the library in an existing project using godot-rust,
+//! remember to add this to your root crate's `lib.rs`:
+//! ```
+//! extern crate hooke_spring;
+//! ```
+//! 
+//! For examples within Godot, please refer to `/examples/godot_bind` in the source code
 //! as they are written in GDScript.
+//! 
+//! The `gdextension` `entry_symbol` of this library is `rs_hooke_spring_lib`.
 use godot::prelude::*;
 
 pub struct GDHookeSpringExtension;
+#[gdextension(entry_symbol = rs_hooke_spring_lib)]
 unsafe impl ExtensionLibrary for GDHookeSpringExtension {}
 
 pub mod rs_hooke_spring;
